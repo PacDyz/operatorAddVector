ac074c84b411d13de08243f43da61fa7ce6a4394
PacDyz <lorako111@gmail.com>
2017-07-09 02:39:25 +0200

    Fix bug (function return vector correctly)
    Fix "return function" from void to std::vector.

--- operatorAddVector.cpp
+++ operatorAddVector.cpp
@@ -4,18 +4,27 @@
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
-template<typename T>
-std::vector<T> operator+( std::vector<T> vec1, std::vector<T> vec2)
+#include <type_traits>
+#include <algorithm>
+#include <utility>
+
+template<typename> struct is_std_vector :  std::false_type {};
+template<typename T, typename A> struct is_std_vector<std::vector<T, A>> :  std::true_type {};
+template <typename T>
+std::enable_if_t<is_std_vector<std::decay_t<T>>::value, T>
+operator+(T&& vec1, T&& vec2)
 {
-	vec1.insert(vec1.end(), vec2.begin(), vec2.end());
+	for (int i = 0; i < vec2.size(); ++i)
+		vec1.emplace_back(std::forward<int>(vec2.at(i)));
 	return vec1;
 }
+
 int main()
 {
 	std::vector<int> vec1{ 1,2,3,4,5 };
 	std::vector<int> vec2{ 2,3,4,5,6 };
 	std::vector<int> vecResult;
-	vecResult = vec1 + vec2;
+	vecResult = std::move(vec1) + std::move(vec2);
 	for (const auto& itr : vecResult)
 		std::cout << itr << std::endl;
     return 0;
